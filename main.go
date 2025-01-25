66192163fab1c8b1f40a13aa32f1f0b2a1d34774
musaubrian <musaubrian15@gmail.com>
2025-01-25 17:21:41 +0300

    feat: get renderer working kinda

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"log/slog"
 	"os"
+	"path"
 	"strings"
 	"time"
 
 	"github.com/charmbracelet/bubbles/filepicker"
 	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+)
+
+const (
+	picker = "filepicker"
+	md     = "markdown"
 )
 
 type model struct {
@@ -18,6 +26,9 @@ type model struct {
 	selectedFile string
 	quitting     bool
 	err          error
+	currentPage  string
+
+	viewport viewport.Model
 
 	height  int
 	width   int
@@ -42,39 +53,77 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.width = msg.Width
 		m.height = msg.Height
 
+		if m.currentPage == md {
+			m.viewport.Width = msg.Width - 2*m.padding
+			m.viewport.Height = msg.Height - 2*m.padding
+		}
+
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
 			m.quitting = true
 			return m, tea.Quit
+		case "esc":
+			m.currentPage = picker
+			return m, nil
+		case "up", "k":
+			if m.currentPage == md {
+				m.viewport.LineUp(1)
+			}
+		case "down", "j":
+			if m.currentPage == md {
+				m.viewport.LineDown(1)
+			}
+		case "pgup":
+			if m.currentPage == md {
+				m.viewport.LineUp(m.viewport.Height)
+			}
+		case "pgdown":
+			if m.currentPage == md {
+				m.viewport.LineDown(m.viewport.Height)
+			}
 		}
+
 	case clearErrorMsg:
 		m.err = nil
 	}
 
 	var cmd tea.Cmd
+	var cmds []tea.Cmd
 	m.filepicker, cmd = m.filepicker.Update(msg)
 
 	if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
 		m.selectedFile = path
+		m.err = nil
+		m.currentPage = md
 	}
 
 	if didSelect, _ := m.filepicker.DidSelectDisabledFile(msg); didSelect {
 		m.err = errors.New("Only markdown files supported")
 		m.selectedFile = ""
-		return m, tea.Batch(cmd, clearErrorAfter(2*time.Second))
+		return m, tea.Batch(cmd, clearErrorAfter(1*time.Second))
+	}
+	if m.currentPage == md {
+		m.viewport = viewport.New(int(float64(m.width)*0.4), m.height)
+		m.viewport, cmd = m.viewport.Update(msg)
+		cmds = append(cmds, cmd)
+		return m, tea.Batch(cmds...)
 	}
 
 	return m, cmd
 }
 func (m model) View() string {
-	containerStyle := lipgloss.NewStyle().
-		Align(lipgloss.Center).Padding(0, m.padding)
+	switch m.currentPage {
+	case picker:
+		return m.FilePickerView()
+	case md:
+		return m.MarkdownView()
+	}
+	return "you shouldn't be seeing this"
+}
 
-	contentStyle := lipgloss.NewStyle().
-		Width(m.width - 2*m.padding).
-		Height(m.height / 2).
-		Align(lipgloss.Left)
+func (m model) FilePickerView() string {
+	containerStyle := lipgloss.NewStyle().Padding(0, m.padding).Width(int(float64(m.width) * 0.4))
 
 	if m.quitting {
 		return ""
@@ -84,39 +133,59 @@ func (m model) View() string {
 	s.WriteString("\n ")
 	if m.err != nil {
 		s.WriteString(m.filepicker.Styles.DisabledFile.Render(m.err.Error()))
-	} else if m.selectedFile == "" {
-		s.WriteString(" Pick a file:")
 	} else {
-		// Do some rendering [replace | navigate to pager view]
-		s.WriteString(" Selected file: " + m.filepicker.Styles.Selected.Render(m.selectedFile))
+		s.WriteString(" Pick a file:")
 	}
 
 	s.WriteString("\n" + m.filepicker.View() + "\n")
-	paddedContet := containerStyle.Render(contentStyle.Render(s.String()))
-	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, paddedContet)
+	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, containerStyle.Render(s.String()))
+}
+
+func (m model) MarkdownView() string {
+	containerStyle := lipgloss.NewStyle().Padding(0, m.padding).Width(int(float64(m.width) * 0.4))
+	f, err := os.Open(m.selectedFile)
+	if err != nil {
+		return "An error occured:\n\t" + err.Error()
+	}
+	defer f.Close()
+
+	var contents strings.Builder
+	title := path.Base(m.selectedFile)
+	contents.WriteString("\n" + title + "\n\n")
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		contents.WriteString(sc.Text() + "\n")
+	}
+	if sc.Err() != nil {
+		return "An error occured:\n\t" + sc.Err().Error()
+	}
+	m.viewport.SetContent(containerStyle.Render(contents.String()))
+	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, m.viewport.View())
+}
+func renderMD(content string) string {
+	return content
 }
 
 func main() {
 	fp := filepicker.New()
 	fp.AllowedTypes = []string{".md"}
-	fp.CurrentDirectory, _ = os.Getwd()
-	fp.KeyMap = filepicker.KeyMap{
-		GoToTop:  key.NewBinding(key.WithKeys("g"), key.WithHelp("g", "first")),
-		GoToLast: key.NewBinding(key.WithKeys("G"), key.WithHelp("G", "last")),
-		Down:     key.NewBinding(key.WithKeys("j", "down", "ctrl+n"), key.WithHelp("j", "down")),
-		Up:       key.NewBinding(key.WithKeys("k", "up", "ctrl+p"), key.WithHelp("k", "up")),
-		PageUp:   key.NewBinding(key.WithKeys("K", "pgup"), key.WithHelp("pgup", "page up")),
-		PageDown: key.NewBinding(key.WithKeys("J", "pgdown"), key.WithHelp("pgdown", "page down")),
-		Back:     key.NewBinding(key.WithKeys("-", "h", "backspace", "left", "esc"), key.WithHelp("h", "back")),
-		Open:     key.NewBinding(key.WithKeys("l", "right", "enter"), key.WithHelp("l", "open")),
-		Select:   key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "select")),
+	dir, err := os.Getwd()
+	if err != nil {
+		slog.Error(err.Error())
 	}
+	fp.CurrentDirectory = dir
+	fp.KeyMap.Back = key.NewBinding(
+		key.WithKeys("-", "backspace", "left", "esc"),
+		key.WithHelp("h", "back"))
 
 	p := tea.NewProgram(
 		model{
-			filepicker: fp,
-			padding:    5,
+			filepicker:  fp,
+			padding:     5,
+			currentPage: picker,
 		},
+		tea.WithAltScreen(),
+		tea.WithMouseCellMotion(),
 	)
 
 	if _, err := p.Run(); err != nil {
